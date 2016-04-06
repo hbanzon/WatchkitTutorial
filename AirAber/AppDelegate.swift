1a4466a03df127c264b0bfe090a13c8b39515410
Hino Banzon <hino.banzon@brivo.com>
2016-04-05 21:46:48 -0400

    completed tutorial

--- AirAber/AppDelegate.swift
+++ AirAber/AppDelegate.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import WatchConnectivity
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
   
-  var window: UIWindow?
+    var window: UIWindow?
+    
+    var session: WCSession? {
+        didSet {
+            if let session = session {
+                session.delegate = self
+                session.activateSession()
+            }
+        }
+    }
+    
   
-  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    return true
-  }
+    // MARK:
+    //   Called when the application has finished launching
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        if WCSession.isSupported() {
+            session = WCSession.defaultSession()
+        }
+        
+        return true
+    }
 
+}
+
+extension AppDelegate: WCSessionDelegate {
+    
+    func session(session: WCSession, didReceiveMessage message: [String : AnyObject], replyHandler: ([String : AnyObject]) -> Void) {
+        if let reference = message["reference"] as? String, boardingPass = QRCode(reference) {
+            replyHandler(["boardingPassData": boardingPass.PNGData])
+        }
+    }
+    
 }
--- Watch Extension/BoardingPassInterfaceController.swift
+++ Watch Extension/BoardingPassInterfaceController.swift
@@ -8,7 +8,7 @@
 
 import WatchKit
 import Foundation
-
+import WatchConnectivity
 
 class BoardingPassInterfaceController: WKInterfaceController {
 
@@ -22,6 +22,10 @@ class BoardingPassInterfaceController: WKInterfaceController {
                 originLabel.setText(flight.origin)
                 destinationLabel.setText(flight.destination)
             }
+            
+            if let _ = flight?.boardingPass {
+                showBoardingPass()
+            }
         }
     }
     
@@ -29,5 +33,55 @@ class BoardingPassInterfaceController: WKInterfaceController {
         super.awakeWithContext(context)
         if let flight = context as? Flight { self.flight = flight }
     }
+    
+    override func didAppear() {
+        super.didAppear()
+        
+        // if we have a valid flight that has no boarding pass and watch connectivity is suported then send message
+        if let flight = flight where flight.boardingPass == nil && WCSession.isSupported() {
+            session = WCSession.defaultSession() // default singleton
+            
+            // fire off the message to the companion iphone app
+            //  include a dictionary containing the flight reference that will be forwarded to the iPhone app
+            session!.sendMessage(["reference": flight.reference], replyHandler: { (response) -> Void in
+                // reply handler receives a dictionary, and is called by the iPhone app
+                // extract the image data of the boarding pass from the dictionary before attempting to create an instance
+                // of the image with it
+                if let boardingPassData = response["boardingPassData"] as? NSData, boardingPass = UIImage(data: boardingPassData) {
+                    // set the image as the flight's boarding pass and jump over to the main queue where you call showBoardingPass()
+                    flight.boardingPass = boardingPass
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        self.showBoardingPass()
+                    })
+                }
+                
+                }, errorHandler: { (error) -> Void in
+                    print(error)
+            })
+        }
+    }
+    
+    // Watch connectivity session
+    //   All communication between the watch and phone is handled by the WCSession
+    var session: WCSession? {
+        didSet {
+            if let session = session {
+                session.delegate = self
+                session.activateSession()
+            }
+        }
+    }
+    
+    private func showBoardingPass() {
+        boardingPassImage.stopAnimating()
+        boardingPassImage.setWidth(120)
+        boardingPassImage.setHeight(120)
+        boardingPassImage.setImage(flight?.boardingPass)
+    }
+    
+}
+
+
+extension BoardingPassInterfaceController : WCSessionDelegate {
     
 }
